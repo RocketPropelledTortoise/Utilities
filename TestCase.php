486999f401cf4e2444a278729caa5c453866209e
Stéphane Goetz <onigoetz@onigoetz.ch>
2015-03-10 21:23:48 +0100

    Remove useless code

--- TestCase.php
+++ TestCase.php
@@ -109,36 +109,4 @@ class TestCase extends \Orchestra\Testbench\TestCase
             }
         }
     }
-
-    /**
-     * Get package providers. At a minimum this is the package being tested, but also
-     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
-     * In a normal app environment these would be added to the 'providers' array in
-     * the config/app.php file.
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return array(
-            //'Cartalyst\Sentry\SentryServiceProvider',
-            //'YourProject\YourPackage\YourPackageServiceProvider',
-        );
-    }
-
-    /**
-     * Get package aliases. In a normal app environment these would be added to
-     * the 'aliases' array in the config/app.php file. If your package exposes an
-     * aliased facade, you should add the alias here, along with aliases for
-     * facades upon which your package depends, e.g. Cartalyst/Sentry
-     *
-     * @return array
-     */
-    protected function getPackageAliases($app)
-    {
-        return array(
-            //'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
-            //'YourPackage' => 'YourProject\YourPackage\Facades\YourPackage',
-        );
-    }
 }
